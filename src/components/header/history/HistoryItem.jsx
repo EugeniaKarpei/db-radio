fa1896248db6b6542de6dfb06180e98cc9ceefa3
EugeniaKarpei <95495762+EugeniaKarpei@users.noreply.github.com>
2024-01-04 09:40:48 -0500

    fix for copy to clipboard bug

--- src/components/header/history/HistoryItem.jsx
+++ src/components/header/history/HistoryItem.jsx
@@ -1,21 +1,21 @@
 import React from "react"
 import { BiCopy } from "react-icons/bi"
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 function HistoryItem({artist, songTitle, startTime, onAir}){
+    const itemText = `${artist} - ${songTitle}`
 
-    function getArtistAndSong(){
-        return `${artist} - ${songTitle}`
-    }
-
-    function handleClick(){
-        navigator.clipboard.writeText(getArtistAndSong())
-    }
+    // function handleClick(){
+    //     navigator.clipboard.writeText(getArtistAndSong())
+    // }
 
     return(
         <div className={`history-item ${onAir && 'light'}`}>
             <p className="history-item-time">{`${startTime}`}</p>
-            <p className="history-item-song">{getArtistAndSong()}</p>
-            <BiCopy className={`copy-btn ${onAir && 'light'}`} onClick={handleClick} title="copy song name"/>
+            <p className="history-item-song">{itemText}</p>
+            <CopyToClipboard text={itemText}>
+                <BiCopy className={`copy-btn ${onAir && 'light'}`} title="copy song name"/>
+            </CopyToClipboard>
         </div>
     )
 }
